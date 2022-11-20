74f977cbad265eb802f3a881f9c791acf1419b41
Бауыржан Нагизханов <nagyzkhanov.b.k@gmail.com>
2022-11-21 01:27:08 +0600

    First commit

---
+++ Uploader/forms.py
@@ -0,0 +1,14 @@
+
+# forms.py
+from django import forms
+from .models import UploadExcel
+from django.forms import ClearableFileInput
+
+
+class FileFieldForm(forms.ModelForm):
+    class Meta:
+        model = UploadExcel
+        fields = ['file_field']
+        widgets = {
+            'file_field': ClearableFileInput(attrs={'multiple': True}),
+        }
---
+++ Uploader/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+from django.core.validators import FileExtensionValidator
+
+
+class Employee(models.Model):
+    employee_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.employee_name
+
+    objects = models.Manager()
+
+
+class ACS(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, null=True)
+    company = models.CharField(max_length=50)
+    datetime = models.DateTimeField()
+    direction = models.CharField(max_length=5)
+    door = models.CharField(max_length=50)
+
+    objects = models.Manager()
+
+
+class UploadExcel(models.Model):
+    file_field = models.FileField(upload_to='media/', blank=True, null=True,
+                                  validators=[FileExtensionValidator(['xlsx'])])
